cf7adf4a02abb1393822581f643a50662057bef0
Aadarsh805 <aadarshthakur80501@gmail.com>
2024-11-17 01:12:16 +0700

    feat: sidebar active state

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { RabotIcon, SettingsIcon } from "./icons";
 import { Button } from "./ui/button";
 import { FileText } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface Button {
   id: number;
@@ -14,6 +15,8 @@ interface Button {
 }
 
 const Sidebar = () => {
+  const path = usePathname();
+
   const [activeButton, setActiveButton] = useState<number | null>(1);
   const buttons: Button[] = [
     {
@@ -28,19 +31,23 @@ const Sidebar = () => {
 
   return (
     <div className="py-6 px-6 bg-[#121212] rounded-md w-fit flex flex-col items-start gap-6 min-w-60">
-      {buttons.map((button) => (
-        <Link
-          href={button.link}
-          key={button.id}
-          className={` flex items-center justify-center gap-2  transition-colors duration-200 hover:bg-none  ${
-            activeButton === button.id ? " text-[#FF5900] " : " text-white "
-          }`}
-          onClick={() => setActiveButton(button.id)}
-        >
-          <button.icon className="inline-block w-fit flex-shrink-0 " />
-          <p className="capitalize">{button.label}</p>
-        </Link>
-      ))}
+      {buttons.map((button) => {
+        const isActive = path === button.link;
+
+        return (
+          <Link
+            href={button.link}
+            key={button.id}
+            className={` flex items-center justify-center gap-2  transition-colors duration-200 hover:bg-none  ${
+              isActive ? " text-accent" : " text-white hover:text-accent"
+            }`}
+            onClick={() => setActiveButton(button.id)}
+          >
+            <button.icon className="inline-block w-fit flex-shrink-0 " />
+            <p className="capitalize">{button.label}</p>
+          </Link>
+        );
+      })}
     </div>
   );
 };
